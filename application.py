e7bbd4eeb0f1990c47f45929956675eeaf1329c9
Vaibhav Sharma <vaibhav140997@gmail.com>
2023-07-14 13:33:10 +0530

    Another set of changes

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Hello Best Bike App! Again Lets see!</h1></body></html>\n"
+    return "<html><body><h1>Hello Best Bike App! Again Lets see!OMG Its working!!</h1></body></html>\n"
 
 
 if __name__ == "__main__":
